ffecbc654d84fff6041891a343033c0dcf9ff53d
Gideon Sireling <code@accursoft.com>
2012-01-22 12:58:50 +0000

    fixed identity column in IdRanges

--- Database/Schema Objects/Tables/IdRanges.sql
+++ Database/Schema Objects/Tables/IdRanges.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[IdRanges]
 (
-    [Id] INT NOT NULL PRIMARY KEY, 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Machine] NCHAR(25) NOT NULL, 
     [Min] INT NOT NULL, 
     [Max] INT NOT NULL
